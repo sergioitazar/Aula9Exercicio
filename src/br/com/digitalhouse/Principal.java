895cdf633f4268d7099c8fb302bc4397dcb16297
sergioitazar <itazar@gmail.com>
2019-04-15 22:23:27 -0300

    alteracao da classe principal

--- src/br/com/digitalhouse/Principal.java
+++ src/br/com/digitalhouse/Principal.java
@@ -39,5 +39,47 @@ public class Principal {
         for (String chave : pessoaMap.keySet()){
             System.out.println("Nome: " + pessoaMap.get(chave).getNome()  + " \nSobrenome: " + pessoaMap.get(chave).getSobreNome() + "\n");
         }
+
+        //loteria dos sonhos exerc1 aula 9
+        Map<Integer,String> loteriaDosSonhos = new HashMap<>();
+        loteriaDosSonhos.put(0,"Ovos");
+        loteriaDosSonhos.put(1,"Agua");
+        loteriaDosSonhos.put(2,"Escopeta");
+        loteriaDosSonhos.put(3,"Cavalo");
+        loteriaDosSonhos.put(4,"Dentista");
+        loteriaDosSonhos.put(5,"Fogo");
+
+        for (int chave : loteriaDosSonhos.keySet()){
+            System.out.println("Valor da loteria: " + loteriaDosSonhos.get(chave)) ;
+        }
+
+        // exercicio 2 item 3
+        List<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(5);
+        lista.add(5);
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(8);
+        lista.add(8);
+        for (Integer list : lista) {
+            System.out.println(list);
+        }
+        // exercicio 2 item 4
+
+        Set<Integer> conjunto = new HashSet<>();
+        conjunto.add(1);
+        conjunto.add(5);
+        conjunto.add(5);
+        conjunto.add(6);
+        conjunto.add(7);
+        conjunto.add(8);
+        conjunto.add(8);
+        conjunto.add(8);
+
+        for (Integer conjuntos : conjunto) {
+            System.out.println(conjuntos);
+        }
     }
 }
